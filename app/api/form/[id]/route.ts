cf3818a64d3b4bbb52ec57cd46f5390773c5682b
myselfkhushboo <khushboocsjm@gmail.com>
2025-01-25 22:02:53 +0530

    kkk

--- app/api/form/[id]/route.ts
+++ app/api/form/[id]/route.ts
@@ -2,34 +2,37 @@ import { connectDB } from "@/app/database/connectBD";
 import { Data } from "@/app/database/schema";
 import { NextRequest, NextResponse } from "next/server";
 
-type ParamType = {
-    params: {
-        id: string;
-    };
-};
-
-export const GET = async (req: NextRequest, { params }: ParamType) => {
-    // return NextResponse.json("hello")
+ const GET = async (req: NextRequest, context: { params: { id: string } }) => {
     try {
-        const { id } = params;
-        await connectDB();
-        const data = await Data.findById({_id:id});
+        const { id } = context.params; 
+        await connectDB(); 
+
+        const data = await Data.findById(id);
+
         if (!data) {
-            return NextResponse.json({
-                status: 'fail',
-                message: 'Form not found',
-            });
+            return NextResponse.json(
+                {
+                    status: "fail",
+                    message: "Form not found",
+                },
+                { status: 404 }
+            );
         }
+
         return NextResponse.json({
-            status: 'success',
-            message: 'Form found',
-            data
+            status: "success",
+            message: "Form found",
+            data,
         });
     } catch (error) {
         console.error("Error fetching form:", error);
-        return NextResponse.json({
-            status: 'error',
-            message: 'An error occurred',
-        });
+        return NextResponse.json(
+            {
+                status: "error",
+                message: "An error occurred",
+            },
+            { status: 500 }
+        );
     }
 };
+export {GET}
