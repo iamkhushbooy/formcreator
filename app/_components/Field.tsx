5a61290d07a672944bfc7ed5d54fbbb02bbc6fd9
myselfkhushboo <khushboocsjm@gmail.com>
2025-01-26 11:57:08 +0530

    add

--- app/_components/Field.tsx
+++ app/_components/Field.tsx
@@ -12,6 +12,7 @@ export type FieldType = {
 const Field = () => {
   const [showField, setShowField] = useState<FieldType[]>([]);
   const [email,setEmail]=useState('')
+  const [linkgenerated,setLinkgenerated]=useState(false)
 
   const handleShowField = () => {
     setShowField((prevFields) => [
@@ -39,54 +40,64 @@ const Field = () => {
       const res = await axios.post('api/save', {
         showField, email
       })
-      alert('link genrated')
+      setLinkgenerated(true)
     } catch (error) {
       alert('error occured')
     }
   }
 
   return (
-    <div
-      className="bg-slate-50 border border-blue-200 w-[90%] h-[70%] rounded 
-      flex items-center flex-col md:w-[40%] mt-16"
-    >
-      <h1 className="font-semibold text-[22px] mt-5">Form Builder</h1>
-      <button
-        onClick={handleShowField}
-        className="mt-5 bg-blue-300 p-2 rounded-md hover:bg-blue-400"
-      >
-        Add New Field
-      </button>
-      <div
-        className="w-full h-[80%] flex items-center flex-col 
-        overflow-scroll overflow-x-hidden rounded"
-      >
-        {showField.map((item) => (
-          <NewField
-            key={item.id}
-            id={item.id}
-            showField={showField}
-            setShowField={setShowField}
-          />
-        ))}
-      </div>
-      <div>
-        <input type="text" placeholder='write your email'
-          className="m-1 w-[130px] h-[40px] mb-5 border rounded p-2 
-          focus:outline-none focus:ring-0 focus:border-gray-300
-          md:w-[170px] md:p-2"
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
-        />
-        <button onClick={handleDBandMail}
-          className='w-[130px] mt-5 m-1 bg-green-300 rounded-md 
-          hover:bg-green-400 text-[15px] h-[40px]
-           md:w-[170px] md:p-2'>Generate Link
-        </button>
-      </div>
-
-    </div>
-    // {/* */}
+    <>
+    {!linkgenerated?(
+       <div
+       className="bg-slate-50 border border-blue-200 w-[90%] h-[70%] rounded 
+       flex items-center flex-col md:w-[40%] mt-16"
+     >
+       <h1 className="font-semibold text-[22px] mt-5">Form Builder</h1>
+       <button
+         onClick={handleShowField}
+         className="mt-5 bg-blue-300 p-2 rounded-md hover:bg-blue-400"
+       >
+         Add New Field
+       </button>
+       <div
+         className="w-full h-[80%] flex items-center flex-col 
+         overflow-scroll overflow-x-hidden rounded"
+       >
+         {showField.map((item) => (
+           <NewField
+             key={item.id}
+             id={item.id}
+             showField={showField}
+             setShowField={setShowField}
+           />
+         ))}
+       </div>
+       <div>
+         <input type="text" placeholder='write your email'
+           className="m-1 w-[130px] h-[40px] mb-5 border rounded p-2 
+           focus:outline-none focus:ring-0 focus:border-gray-300
+           md:w-[170px] md:p-2"
+           value={email}
+           onChange={(e)=>setEmail(e.target.value)}
+         />
+         <button onClick={handleDBandMail}
+           className='w-[130px] mt-5 m-1 bg-green-300 rounded-md 
+           hover:bg-green-400 text-[15px] h-[40px]
+            md:w-[170px] md:p-2'>Generate Link
+         </button>
+       </div>
+ 
+     </div>
+    ):(
+      <div className='w-full h-screen flex justify-center items-center flex-col'>
+      <h1 className='text-xl font-bold text-green-600
+      md:text-3xl
+      '>Link Generated Successfully!</h1>
+      <p className='text-lg mt-3'>please check your email.</p>
+  </div>
+    )}
+    </>
 
   );
 };
--- app/form/[id]/page.tsx
+++ app/form/[id]/page.tsx
@@ -69,72 +69,74 @@ const Page = () => {
 
     return (
         <>
-        {!formSubmitted?(
-             <form className="relative min-h-screen">
-             <div className="absolute inset-0">
-                 <Image
-                     src="/form.jpeg"
-                     alt="Form Builder Design"
-                     layout="fill"
-                     objectFit="cover"
-                     className="opacity-90"
-                 />
-             </div>
-             <div className="relative flex justify-center items-center flex-col">
-                 {field.map((item, index) => (
-                     <div
-                         key={index}
-                         className="rounded-lg w-[80%] h-[150px] flex flex-col border bg-[#ffffff] my-5
+            {!formSubmitted ? (
+                <form className="relative min-h-screen">
+                    <div className="absolute inset-0">
+                        <Image
+                            src="/form.jpeg"
+                            alt="Form Builder Design"
+                            layout="fill"
+                            objectFit="cover"
+                            className="opacity-90"
+                        />
+                    </div>
+                    <div className="relative flex justify-center items-center flex-col">
+                        {field.map((item, index) => (
+                            <div
+                                key={index}
+                                className="rounded-lg w-[80%] h-[150px] flex flex-col border bg-[#ffffff] my-5
                          border-blue-300"
-                     >
-                         <label htmlFor={`field-${index}`} className="text-[20px] mx-5 mt-5">
-                             {item.label}
-                         </label>
-                         <input
-                             id={`field-${index}`}
-                             type="text"
-                             placeholder="Your answer"
-                             className="w-[90%] h-[35px] border-b-2 p-2 focus:border-b-blue-400 duration-300 focus:outline-none m-5"
-                             value={fieldsave[index]?.value || ''}
-                             onChange={(e) => handleInputChange(index, e.target.value)}
-                             required
-                         />
-                     </div>
-                 ))}
-             </div>
-             <div className="relative w-[80%] h-[80px] flex items-center justify-between font-medium mx-[10%]">
-                 <button
-                 onClick={submitForm}
-                     type="submit"
-                     className="text-[20px] px-5 text-white bg-purple-700 hover:bg-purple-900 rounded-md p-2"
-                 >
-                     Submit
-                 </button>
-                 <button
-                     type="button"
-                     onClick={clearForm}
-                     className="text-purple-900"
-                 >
-                     Clear Form
-                 </button>
-             </div>
-             <div className="relative w-[80%] mx-[10%] flex justify-center items-center flex-col
+                            >
+                                <label htmlFor={`field-${index}`} className="text-[20px] mx-5 mt-5">
+                                    {item.label}
+                                </label>
+                                <input
+                                    id={`field-${index}`}
+                                    type="text"
+                                    placeholder="Your answer"
+                                    className="w-[90%] h-[35px] border-b-2 p-2 focus:border-b-blue-400 duration-300 focus:outline-none m-5"
+                                    value={fieldsave[index]?.value || ''}
+                                    onChange={(e) => handleInputChange(index, e.target.value)}
+                                    required
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="relative w-[80%] h-[80px] flex items-center justify-between font-medium mx-[10%]">
+                        <button
+                            onClick={submitForm}
+                            type="submit"
+                            className="text-[20px] px-5 text-white bg-purple-700 hover:bg-purple-900 rounded-md p-2"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            onClick={clearForm}
+                            className="text-purple-900"
+                        >
+                            Clear Form
+                        </button>
+                    </div>
+                    <div className="relative w-[80%] mx-[10%] flex justify-center items-center flex-col
               ">
-                 <p className="p-2 text-[11px]">This form is created by {email}</p>
-                 <footer className="font-extralight text-[12px]">
-                     This website created by Khushboo
-                 </footer>
-             </div>
-         </form>
-        ):(
-            <div className='w-full h-screen flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-bold text-green-600'>Form Submitted Successfully!</h1>
-            <p className='text-lg mt-3'>Thank you for your response.</p>
-        </div>
-        )
+                        <p className="p-2 text-[11px]">This form is created by {email}</p>
+                        <footer className="font-extralight text-[12px]">
+                            This website created by Khushboo
+                        </footer>
+                    </div>
+                </form>
+            ) : (
+                <div className='w-full h-screen flex justify-center items-center flex-col'>
+                    <h1 className='text-xl font-bold text-green-600
+                    md:text-3xl
+                    '>Form Submitted Successfully!</h1>
+                    <p className='text-lg mt-3'>Thank you for your response.</p>
+                </div>
+            )
 
-        }
-       
+            }
+
         </>
     );
 };
